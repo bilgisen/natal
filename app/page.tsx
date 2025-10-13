428c2d0f0fea7150ef6cc81e5f8b8dcbf37de62c
bilgisen <hbkarabey@gmail.com>
2025-10-14 01:42:25 +0300

    loginonetap

--- app/page.tsx
+++ app/page.tsx
@@ -5,14 +5,12 @@ import { getSubscriptionDetails } from "@/lib/subscription";
 import PricingTable from "./pricing/_component/pricing-table";
 import { CurrentTransitsWidget } from "@/components/astrology/CurrentTransitsWidget";
 import { DailyHoroscopeWidget } from "@/components/astrology/DailyHoroscopeWidget";
-import HomePageClient from "@/components/HomePageClient";
 
 export default async function Home() {
   const subscriptionDetails = await getSubscriptionDetails();
 
   return (
     <>
-      <HomePageClient />
       <HeroSection />
       <Integrations />
       <div className="container mx-auto px-4 py-8 space-y-16">
